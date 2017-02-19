10e82fc862cc2f5e96412b5d7636e70068d339dd
lunarpulse <lunarpulse@gmail.com>
2017-02-20 01:43:52 +1000

    working XOR learning TODO: args, rust,

--- nNc/neuralNetworkN.cpp
+++ nNc/neuralNetworkN.cpp
@@ -1,29 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <cmath>
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
 
 //constants
 const int NUMINPUTNODES = 2;
 const int NUMHIDDENNODES = 2;
 const int NOMOUTPUTNODES = 1;
-const int NUMNODES = NOMOUTPUTNODES + NUMHIDDENNODES + NUMINPUTNODES;
+const int NUMNODES = NUMINPUTNODES + NUMHIDDENNODES + NOMOUTPUTNODES;
 
 const int ARRAYSIZE = NUMNODES + 1; //1 - offset to match node 1 node 2 etc
-const int MAXITERATIONS = 13107;//131072;
+const int MAXITERATIONS = 131072;
 
 const double E = 2.71828;
-const double LEARNINGRATE = 0.1;
+const double LEARNINGRATE = 0.2;
 
 //function prototypes
 void initialise(double[][ARRAYSIZE], double[], double[], double[]);
-void connectNode(double[][ARRAYSIZE], double[]);
+void connectNodes(double[][ARRAYSIZE], double[]);
 void trainingExample(double[], double[]);
 void activateNetwork(double[][ARRAYSIZE], double[], double[]);
 double updateWeights(double[][ARRAYSIZE], double[], double[], double[]);
 void displayNetwork(double[], double);
 
 int main() {
-	printf("Neural Network Program\n");
+
+	std::cout<<"Neural Network Program"<<endl;
 
 	double weights[ARRAYSIZE][ARRAYSIZE];
 	double values[ARRAYSIZE];
@@ -31,7 +36,7 @@ int main() {
 	double thresholds[ARRAYSIZE];
 
 	initialise(weights, values, expectedValues, thresholds);
-	connectNode(weights, thresholds);
+	connectNodes(weights, thresholds);
 	int counter = 0;
 
 	while (counter < MAXITERATIONS) {
@@ -55,10 +60,10 @@ void initialise(double weights[][ARRAYSIZE], double values[], double expectedVal
 	}
 }
 
-void connectNode(double weights[][ARRAYSIZE], double thresholds[]) {
-	for (size_t x = 0; x < NUMNODES; x++) {
-		for (size_t y = 0; y < NUMNODES; y++) {
-			weights[x][y] = (rand() % 200) / 100.0;
+void connectNodes(double weights[][ARRAYSIZE], double thresholds[]) {
+	for (size_t x = 1; x < NUMNODES + 1; x++) {
+		for (size_t y = 1; y < NUMNODES + 1; y++) {
+			weights[x][y] = (rand() % 200) / 200.0 - 0.5;
 		}
 	}
 
@@ -66,7 +71,9 @@ void connectNode(double weights[][ARRAYSIZE], double thresholds[]) {
 	thresholds[4] = rand() / (double)rand();
 	thresholds[5] = rand() / (double)rand();
 
-	printf("%f%f%f%f%f%f\n%f%f%f\n", weights[1][3], weights[1][4], weights[2][3], weights[2][4], weights[3][5], weights[4][5], thresholds[3], thresholds[4], thresholds[5]);
+	cout<< weights[1][3] <<" "<< weights[1][4] << " " << weights[2][3]<<\
+		" " << weights[2][4] << " " << weights[3][5] << " " << weights[4][5] << endl\
+		<< thresholds[3] << " " << thresholds[4] << " " << thresholds[5]<<endl;
 
 }
 
@@ -75,24 +82,24 @@ void trainingExample(double values[], double expectedValues[]) {
 
 	switch (counter % 4) {
 	case 0:
-		values[1] = 1;
-		values[2] = 1;
-		expectedValues[5] = 0;
+		values[1] = 1.0;
+		values[2] = 1.0;
+		expectedValues[5] = 0.0;
 		break;
 	case 1:
-		values[1] = 0;
-		values[2] = 1;
-		expectedValues[5] = 1;
+		values[1] = 0.0;
+		values[2] = 1.0;
+		expectedValues[5] = 1.0;
 		break;
 	case 2:
-		values[1] = 1;
-		values[2] = 0;
-		expectedValues[5] = 1;
+		values[1] = 1.0;
+		values[2] = 0.0;
+		expectedValues[5] = 1.0;
 		break;
 	case 3:
-		values[1] = 0;
-		values[2] = 0;
-		expectedValues[5] = 0;
+		values[1] = 0.0;
+		values[2] = 0.0;
+		expectedValues[5] = 0.0;
 		break;
 	}
 	counter++;
@@ -103,22 +110,23 @@ void activateNetwork(double weights[][ARRAYSIZE], double values[], double thresh
 	for (size_t h = 1 + NUMINPUTNODES; h < 1 + NUMINPUTNODES + NUMHIDDENNODES; h++) {
 		double weightedInput = 0.0;
 		//add up the weihted input
-		for (size_t i = 0; i < NUMINPUTNODES; i++) {
+		for (size_t i = 1; i <1 + NUMINPUTNODES; i++) {
 			weightedInput += weights[i][h] * values[i];
 		}
-		weightedInput += (-1 * thresholds[h]);
+		weightedInput += (-1.0 * thresholds[h]);
 
-		values[h] = 1.0 / (1.0 + pow(E, weightedInput));
+		double tempValue = 1.0 / (1.0 + pow(E, -weightedInput));
+		values[h] = tempValue;// 1.0 / (1.0 + pow(E, -weightedInput));
 	}
-	for (size_t out = 1 + NUMINPUTNODES; out < 1 + NUMNODES; out++) {
+	for (size_t out = 1 + NUMINPUTNODES + NUMHIDDENNODES; out < 1 + NUMNODES; out++) {
 		double weightedInput = 0.0;
-		for (size_t h = 0; h < 1 + NUMINPUTNODES + NUMHIDDENNODES; h++) {
+		for (size_t h = 1 + NUMINPUTNODES; h < 1 + NUMINPUTNODES + NUMHIDDENNODES; h++) {
 			weightedInput += weights[h][out] * values[h];
 		}
 		//handle the thresholds
-		weightedInput += (-1 * thresholds[out]);
-
-		values[out] = 1.0 / (1.0 + pow(E, -weightedInput));
+		weightedInput += (-1.0 * thresholds[out]);
+		double tempValue = 1.0 / (1.0 + pow(E, -weightedInput));
+		values[out] = tempValue; // 1.0 / (1.0 + pow(E, -weightedInput));
 	}
 }
 
@@ -132,18 +140,18 @@ double updateWeights(double weights[][ARRAYSIZE], double values[], double expect
 
 		//update each weigting from the hidden layer
 		for (size_t h = 1 + NUMINPUTNODES; h < 1 + NUMINPUTNODES + NUMHIDDENNODES; h++) {
-			double delta = LEARNINGRATE* values[h] * outputErrorGradient;
+			double delta = LEARNINGRATE * values[h] * outputErrorGradient;
 			weights[h][o] += delta;
-			double hiddenErrorGradient = values[h] * (1 - values[h]) * outputErrorGradient * weights[h][o];
+			double hiddenErrorGradient = values[h] * (1.0 - values[h]) * outputErrorGradient * weights[h][o];
 
-			for (size_t i = 0; i < 1 + NUMINPUTNODES; i++) {
+			for (size_t i = 1; i < 1 + NUMINPUTNODES; i++) {
 				double delta = LEARNINGRATE * values[i] * hiddenErrorGradient;
 				weights[i][h] += delta;
 			}
-			double thresholDelta = LEARNINGRATE * -1 * hiddenErrorGradient;
+			double thresholDelta = LEARNINGRATE * -1.0 * hiddenErrorGradient;
 			thresholds[h] += thresholDelta;
 		}
-		double delta = LEARNINGRATE * -1 * outputErrorGradient;
+		double delta = LEARNINGRATE * -1.0 * outputErrorGradient;
 		thresholds[o] += delta;
 	}
 	return sumOfSquaredErrors;
@@ -151,10 +159,9 @@ double updateWeights(double weights[][ARRAYSIZE], double values[], double expect
 
 void displayNetwork(double values[], double sumOfSquaredErrors) {
 	static int counter = 0;
-	if ((counter % 4) == 0) printf("---------------------------------------------------\n");
-	printf("%8.4f|", values[1]);
-	printf("%8.4f|", values[2]);
-	printf("%8.4f|", values[5]);
-	printf(" error:%8.5f\n", sumOfSquaredErrors);
+	if ((counter % 4) == 0) cout << setfill('-')<< setw(40) << "-" << endl;
+	cout << setfill(' ')<< fixed << setw(8) << setprecision(4)  << values[1] \
+		<< "|" << setprecision(4) << values[2] << "|" << setprecision(4) \
+		<< values[5] << "|" << " error: " <<setprecision(5)	<< sumOfSquaredErrors << endl;
 	counter++;
 }
